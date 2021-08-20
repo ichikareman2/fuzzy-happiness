806e82f6f65a3be276964c78c3e6cb1db3685146
kevin quing <willardquing@gmail.com>
2021-08-20 23:16:44 +0800

    Update
    * add test for button using spectator

--- projects/fuzzy-happiness-lib/src/lib/components/button/button.component.spec.ts
+++ projects/fuzzy-happiness-lib/src/lib/components/button/button.component.spec.ts
@@ -1,25 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
+import { Spectator, createComponentFactory } from '@ngneat/spectator'
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <button lib-button="blue">Click me!</button>
+  `
+})
+class TestComponent {}
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
+  let spectator: Spectator<TestComponent>;
+  const createComponent = createComponentFactory({
+    component: TestComponent,
+    declarations: [ButtonComponent]
   });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.query('button')).toBeTruthy();
+  });
+  it('should have `bg-blue-600` class', () => {
+    expect(spectator.query('button')).toHaveClass('bg-blue-600');
   });
 });
